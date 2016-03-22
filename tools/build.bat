7642162fb626b1f2dbd2ce409e9b359d2c89fefc
Eduardo Dias da Costa <eduardo@thelaborat.org>
2016-03-22 14:56:05 -0300

    update> docs update> suit.js will not embed documentation now.

--- tools/build.bat
+++ tools/build.bat
@@ -1,2 +1,3 @@
-cmd /c jsdoc ./js/suit.js -d docs/deploy/ -t docs/template/ -c docs/template/conf.json -r README.md
-cmd /c uglifyjs js/suit.js -o js/suit.min.js
+cmd /c jsdoc ./js/suit.doc.js -d docs/deploy/ -t docs/template/ -c docs/template/conf.json -r README.md
+cmd /c uglifyjs js/suit.doc.js -b -o js/suit.js
+cmd /c uglifyjs js/suit.doc.js -o js/suit.min.js
